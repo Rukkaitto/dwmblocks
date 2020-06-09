8dce89401997dc3e1632951347666aa595a6d08b
Lucas Goudin <goudinlucas@gmail.com>
2020-06-09 16:35:25 +0200

    New desktop branch

--- blocks.h
+++ blocks.h
@@ -3,11 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/.local/bin/status-modules/updates",					   3600,	               0},
 	{"", "~/.local/bin/status-modules/volume",					      0,		           20},
-	{"", "~/.local/bin/status-modules/battery",					     10,	              21},
-	{"", "~/.local/bin/status-modules/wifi",					        60,	              22},
+	//{"", "~/.local/bin/status-modules/battery",					     10,	              21},
+	//{"", "~/.local/bin/status-modules/wifi",					        60,	              22},
 	{"", "date +%d/%m/%y",				                              0,		            0},
 	{"", "date +%H:%M",					                             60,		            0},
-   {"", "~/.local/bin/status-modules/power",                                                      0,               23},
+   //{"", "~/.local/bin/status-modules/power",                                                      0,               23},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
